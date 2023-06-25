ce2acf25efa056b2fd91bb81f591d3b68989a200
mohdfaiz <mohdfaizlari@gmail.com>
2023-06-25 18:53:59 +0530

    Table created

--- index.js
+++ index.js
@@ -1,278 +1,376 @@
-let inputJson = [
-    {
-        "id": "1",
-        "name": "Aaron Miles",
-        "email": "aaron@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "2",
-        "name": "Aishwarya Naik",
-        "email": "aishwarya@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "3",
-        "name": "Arvind Kumar",
-        "email": "arvind@mailinator.com",
-        "role": "admin"
-    },
-    {
-        "id": "4",
-        "name": "Caterina Binotto",
-        "email": "caterina@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "5",
-        "name": "Chetan Kumar",
-        "email": "chetan@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "6",
-        "name": "Jim McClain",
-        "email": "jim@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "7",
-        "name": "Mahaveer Singh",
-        "email": "mahaveer@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "8",
-        "name": "Rahul Jain",
-        "email": "rahul@mailinator.com",
-        "role": "admin"
-    },
-    {
-        "id": "9",
-        "name": "Rizan Khan",
-        "email": "rizan@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "10",
-        "name": "Sarah Potter",
-        "email": "sarah@mailinator.com",
-        "role": "admin"
-    },
-    {
-        "id": "11",
-        "name": "Keshav Muddaiah",
-        "email": "keshav@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "12",
-        "name": "Nita Ramesh",
-        "email": "nita@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "13",
-        "name": "Julia Hunstman",
-        "email": "julia@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "14",
-        "name": "Juan Alonso",
-        "email": "juan@mailinator.com",
-        "role": "admin"
-    },
-    {
-        "id": "15",
-        "name": "Gabriel Montoya",
-        "email": "gabriel@mailinator.com",
-        "role": "admin"
-    },
-    {
-        "id": "16",
-        "name": "Beatrice Iglesias",
-        "email": "beatrice@mailinator.com",
-        "role": "admin"
-    },
-    {
-        "id": "17",
-        "name": "Sarah Symms",
-        "email": "sarah.s@mailinator.com",
-        "role": "admin"
-    },
-    {
-        "id": "18",
-        "name": "Patrick Pinheiro",
-        "email": "patrick@mailinator.com",
-        "role": "admin"
-    },
-    {
-        "id": "19",
-        "name": "Anand Patel",
-        "email": "anand@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "20",
-        "name": "Kishore Kalburgi",
-        "email": "kishore@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "21",
-        "name": "Rebecca Norris",
-        "email": "rebecca@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "22",
-        "name": "Özgür Başak",
-        "email": "ozgur@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "23",
-        "name": "Robin Andersen",
-        "email": "robin@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "24",
-        "name": "Nandini Kumar",
-        "email": "nandini@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "25",
-        "name": "Nikita Smith",
-        "email": "nikita@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "26",
-        "name": "Colton Doe",
-        "email": "colton@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "27",
-        "name": "Alain Senna",
-        "email": "alain@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "28",
-        "name": "Ashwin Jain",
-        "email": "ashwin@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "29",
-        "name": "Seema Bhatt",
-        "email": "seema@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "30",
-        "name": "Kayla Scarpinski",
-        "email": "kayla@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "31",
-        "name": "Ajay Ghosh",
-        "email": "ajay@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "32",
-        "name": "Chris Lindberg",
-        "email": "chris@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "33",
-        "name": "Christina Mourujärvi",
-        "email": "christina@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "34",
-        "name": "Mikhail Bill",
-        "email": "mikhail@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "35",
-        "name": "Eino Göregen",
-        "email": "eino@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "36",
-        "name": "Zachariah Johansson",
-        "email": "zacharaiah@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "37",
-        "name": "Aimaan Mohammed",
-        "email": "aimaan@mailinator.com",
-        "role": "admin"
-    },
-    {
-        "id": "38",
-        "name": "Aika Tsunoda",
-        "email": "aika@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "39",
-        "name": "Kimiko Minamoto",
-        "email": "kimiko@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "40",
-        "name": "Alyona Baginskaite",
-        "email": "alyona@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "41",
-        "name": "Anirudh Mukherjee",
-        "email": "anirudh@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "42",
-        "name": "Alyona Gov",
-        "email": "alyonagov@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "43",
-        "name": "Robin Singh",
-        "email": "robin@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "44",
-        "name": "Vijay Vasudevan",
-        "email": "vijayv@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "45",
-        "name": "Steve Smith",
-        "email": "steve@mailinator.com",
-        "role": "member"
-    },
-    {
-        "id": "46",
-        "name": "Anirudh Banerjee",
-        "email": "anirudhb@mailinator.com",
-        "role": "member"
-    }
-]
+let rowsData = [
+  {
+    id: "1",
+    name: "Aaron Miles",
+    email: "aaron@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "2",
+    name: "Aishwarya Naik",
+    email: "aishwarya@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "3",
+    name: "Arvind Kumar",
+    email: "arvind@mailinator.com",
+    role: "admin",
+  },
+  {
+    id: "4",
+    name: "Caterina Binotto",
+    email: "caterina@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "5",
+    name: "Chetan Kumar",
+    email: "chetan@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "6",
+    name: "Jim McClain",
+    email: "jim@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "7",
+    name: "Mahaveer Singh",
+    email: "mahaveer@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "8",
+    name: "Rahul Jain",
+    email: "rahul@mailinator.com",
+    role: "admin",
+  },
+  {
+    id: "9",
+    name: "Rizan Khan",
+    email: "rizan@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "10",
+    name: "Sarah Potter",
+    email: "sarah@mailinator.com",
+    role: "admin",
+  },
+  {
+    id: "11",
+    name: "Keshav Muddaiah",
+    email: "keshav@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "12",
+    name: "Nita Ramesh",
+    email: "nita@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "13",
+    name: "Julia Hunstman",
+    email: "julia@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "14",
+    name: "Juan Alonso",
+    email: "juan@mailinator.com",
+    role: "admin",
+  },
+  {
+    id: "15",
+    name: "Gabriel Montoya",
+    email: "gabriel@mailinator.com",
+    role: "admin",
+  },
+  {
+    id: "16",
+    name: "Beatrice Iglesias",
+    email: "beatrice@mailinator.com",
+    role: "admin",
+  },
+  {
+    id: "17",
+    name: "Sarah Symms",
+    email: "sarah.s@mailinator.com",
+    role: "admin",
+  },
+  {
+    id: "18",
+    name: "Patrick Pinheiro",
+    email: "patrick@mailinator.com",
+    role: "admin",
+  },
+  {
+    id: "19",
+    name: "Anand Patel",
+    email: "anand@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "20",
+    name: "Kishore Kalburgi",
+    email: "kishore@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "21",
+    name: "Rebecca Norris",
+    email: "rebecca@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "22",
+    name: "Özgür Başak",
+    email: "ozgur@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "23",
+    name: "Robin Andersen",
+    email: "robin@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "24",
+    name: "Nandini Kumar",
+    email: "nandini@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "25",
+    name: "Nikita Smith",
+    email: "nikita@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "26",
+    name: "Colton Doe",
+    email: "colton@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "27",
+    name: "Alain Senna",
+    email: "alain@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "28",
+    name: "Ashwin Jain",
+    email: "ashwin@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "29",
+    name: "Seema Bhatt",
+    email: "seema@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "30",
+    name: "Kayla Scarpinski",
+    email: "kayla@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "31",
+    name: "Ajay Ghosh",
+    email: "ajay@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "32",
+    name: "Chris Lindberg",
+    email: "chris@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "33",
+    name: "Christina Mourujärvi",
+    email: "christina@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "34",
+    name: "Mikhail Bill",
+    email: "mikhail@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "35",
+    name: "Eino Göregen",
+    email: "eino@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "36",
+    name: "Zachariah Johansson",
+    email: "zacharaiah@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "37",
+    name: "Aimaan Mohammed",
+    email: "aimaan@mailinator.com",
+    role: "admin",
+  },
+  {
+    id: "38",
+    name: "Aika Tsunoda",
+    email: "aika@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "39",
+    name: "Kimiko Minamoto",
+    email: "kimiko@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "40",
+    name: "Alyona Baginskaite",
+    email: "alyona@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "41",
+    name: "Anirudh Mukherjee",
+    email: "anirudh@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "42",
+    name: "Alyona Gov",
+    email: "alyonagov@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "43",
+    name: "Robin Singh",
+    email: "robin@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "44",
+    name: "Vijay Vasudevan",
+    email: "vijayv@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "45",
+    name: "Steve Smith",
+    email: "steve@mailinator.com",
+    role: "member",
+  },
+  {
+    id: "46",
+    name: "Anirudh Banerjee",
+    email: "anirudhb@mailinator.com",
+    role: "member",
+  },
+];
+// let rowsData = [];
+let createTable = () => {
+  /* ----> This condition is checking wheter the input file contains some data or it's blank only
+  according to that the code will work --------> */
+  if (!rowsData.length) {
+    //---- If rowsData length is 0 it will return No data to show!
+    let noDataDiv = document.createElement("div");
+    noDataDiv.style.width = "100%";
+    noDataDiv.style.height = "100%";
+    noDataDiv.innerText = "No data to show! 😑";
+    noDataDiv.style.textAlign = "center";
+    noDataDiv.style.fontWeight = "bolder";
+    noDataDiv.style.marginTop = "20%";
+    let tableBody = document.getElementById("tableBody");
+    tableBody.appendChild(noDataDiv);
+    return;
+  }
+
+  //-------------------------This loop is running to create new rows inside the HTML page---------------------->
+
+  for (let i = 0; i < rowsData.length; i++) {
+    let rowData = rowsData[i];
+
+    let name = rowData.name;
+    let nameSpan = document.createElement("span");
+    nameSpan.innerText = name;
+
+    let email = rowData.email;
+    let emailSpan = document.createElement("span");
+    emailSpan.innerText = email;
+
+    let role = rowData.role;
+    let roleSpan = document.createElement("span");
+    roleSpan.innerText = role;
+
+    let tableRow = document.createElement("div"); // ---> To create new div ele.
+    tableRow.classList.add("tableRows"); //----> To add class in newly created div ele in JS.
+    // <-------------------Method-2-------------------------------->
+    // element.setAttribute()
+    // setAttribute('class', 'fixedWidth');
+
+    let checkBoxCell = document.createElement("div"); //-----------> Creating div inside the new created row
+    checkBoxCell.classList.add("tableRowCells");
+    let inputEle = document.createElement("input");
+    inputEle.style.width = "15px";
+    inputEle.style.height = "15px";
+    inputEle.setAttribute("type", "checkbox");
+    checkBoxCell.appendChild(inputEle);
+    tableRow.appendChild(checkBoxCell);
+
+    let nameCell = document.createElement("div");
+    nameCell.classList.add("tableRowCells");
+    nameCell.appendChild(nameSpan);
+    tableRow.appendChild(nameCell);
+
+    let emailCell = document.createElement("div");
+    emailCell.classList.add("tableRowCells");
+    emailCell.appendChild(emailSpan);
+    tableRow.appendChild(emailCell);
+
+    let roleCell = document.createElement("div");
+    roleCell.classList.add("tableRowCells");
+    roleCell.appendChild(roleSpan);
+    tableRow.appendChild(roleCell);
+
+    //--------------------------Action Cell -------------------------->
+
+    let actionCell = document.createElement("div");
+    
+    actionCell.style.display = "flex";
+    actionCell.style.alignItems = "center";
+    // actionCell.style.justifyContent = "center";
+    actionCell.style.gap = "10px"
+
+    let editButton = document.createElement("button");
+    editButton.style.padding = "5px 8px 5px 8px";
+    // editButton.style.height = "25px";
+    editButton.style.borderRadius = "5px";
+    editButton.innerText = "Edit";
+    editButton.setAttribute("type", "Button");
+    actionCell.appendChild(editButton);
+    
+    let deleteButton = document.createElement("button");
+    deleteButton.style.padding = "5px 8px 5px 8px";
+    // deleteButton.style.height = "25px";
+    deleteButton.style.borderRadius = "5px";
+    deleteButton.setAttribute("type", "Button")
+    deleteButton.innerText = "Delete";
+    actionCell.appendChild(deleteButton);
+
+    actionCell.classList.add("tableRowCells");
+    tableRow.appendChild(actionCell);
+
+    let tableBody = document.getElementById("tableBody");
+    tableBody.appendChild(tableRow);
+  }
+};
+createTable();
